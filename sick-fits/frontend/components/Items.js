ed459757197c6755067a879529dd2ff0d772d0cd
Jarren Bird <adiofan@gmail.com>
2018-10-19 16:18:14 -0600

    added the ability to add items to the store as well as list those items out on the shop page and give those items their own individual pages

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
-import { Query } from 'react-apollo';
-import gql from 'graphql-tag';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import styled from "styled-components";
+import Item from "./Item";
 
 const ALL_ITEMS_QUERY = gql`
   query ALL_ITEMS_QUERY {
@@ -22,7 +23,7 @@ const Center = styled.div`
 
 const ItemsList = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr;
   grid-gap: 60px;
   max-width: ${props => props.theme.maxWidth};
   margin: 0 auto;
@@ -32,21 +33,23 @@ class Items extends Component {
   render() {
     return (
       <Center>
-        <p>Items!</p>
         <Query query={ALL_ITEMS_QUERY}>
           {({ data, error, loading }) => {
-            if (loading) return <p>loading...</p>
-            if (error) return <p>error: {error.message}</p>
+            if (loading) return <p>loading...</p>;
+            if (error) return <p>error: {error.message}</p>;
             return (
               <ItemsList>
-                {data.items.map(item => <p>{item.title}</p>)}
+                {data.items.map(item => (
+                  <Item item={item} key={item.id} />
+                ))}
               </ItemsList>
-            )
+            );
           }}
         </Query>
       </Center>
-    )
+    );
   }
 }
 
 export default Items;
+export { ALL_ITEMS_QUERY };
